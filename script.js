3445f46d0d3aa5d69ce9be84fc50dbd3964725e5
ExperimentalFish <experimentalfish123@gmail.com>
2024-05-12 20:46:31 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
 document.addEventListener('DOMContentLoaded', () => {
     const cursor = document.querySelector('.cursor');
     let isMouseDown = false;
-
+    cursor.style.display = 'none'
+    
     document.addEventListener('mousemove', e => {
       cursor.style.left = e.pageX + 'px';
       cursor.style.top = e.pageY + 'px';
+      cursor.style.display = 'block'; // Display cursor on mouse move
     });
-
     document.addEventListener('mousedown', () => {
       isMouseDown = true;
       cursor.classList.add('cursor-clicked');
       cursor.style.transform = 'scale(1.5)';
     });
-
     document.addEventListener('mouseup', () => {
       isMouseDown = false;
       cursor.style.transform = 'scale(1)';
       cursor.classList.remove('cursor-clicked');
+    });
+    document.addEventListener('mouseleave', () => {
+      cursor.style.display = 'none'; // Hide cursor when mouse leaves the window
     });
 });
